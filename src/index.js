87e275e91a85d35eb4312a59fa264bd2f2d16bb5
omii-143 <omkarpaygude1109@gmail.com>
2021-11-25 12:22:58 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Calc from './Calc';
+import './index.css';
+
+
+ReactDom.render(
+  <>
+  <Calc/>
+  </>,
+  document.getElementById('root')
+);
+
